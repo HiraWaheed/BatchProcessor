782105a40884fbbee55f3fd83edde4518123c047
HiraWaheed <hirawaheed191@gmail.com>
2025-02-04 03:17:49 +0500

    Check batch job status

--- gpt.py
+++ gpt.py
@@ -1,9 +1,12 @@
 
+import json
 import logging
 import os
 from prompt import build_gpt_prompt
 from openai import OpenAI
 from dotenv import load_dotenv
+
+from utils import writing_batch_results
 load_dotenv()
 
 OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
@@ -84,4 +87,66 @@ def create_batch_job(batch_file):
         completion_window="24h"
     )
     return batch_job
+
+
+def check_batch_status(id):
+    """
+    Checks the status of a batch job.
+    Args:
+        id: the ID of the batch job to check
+    """
+    logging.info("Checking batch status")
+    batch_job = client.batches.retrieve(id)
+    print(f"Status for {batch_job} is {batch_job.status}")
+    return batch_job.status
+
+
+def retrieve_batch_results(model, batch_job):
+    """
+    Retrieves the results of a batch job.
+    Args:
+        model: the model used for the batch job
+        batch_job: the batch job to retrieve results from
+    """
+    try:
+        logging.info("Retrieving batch results")
+        if batch_job.output_file_id is None:
+            logging.error("Batch job has no output file")
+            return None
+        
+        result_file_id = batch_job.output_file_id
+        result = client.files.content(result_file_id).content
+        result_file_name = writing_batch_results(model, result)
+        # Loading data from saved file
+        results = []
+        with open(result_file_name, 'r') as file:
+            for line in file:
+                # Parsing the JSON string into a dict and appending to the list of results
+                json_object = json.loads(line.strip())
+                results.append(json_object)
+
+        return results
+    except Exception as e:
+        print(f"Error retrieving batch results: {e}")
+        return None
+
+
+
+def fetch_batch_details():
+    """
+    Fetches details of the most recent batch job.
+    """
+    return client.batches.list(limit=1)
+
+
+def read_file_contents(file_id):
+    """
+    Reads the contents of a file.
+    Args:
+        file_id: the ID of the file to read    
+    """
+    file_response = client.files.content(file_id)
+    print(file_response.text)
+
+
 
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import json
+import logging
+
+from gpt import retrieve_batch_results
 
 
 def write_tasks_to_json(tasks, output_file):
@@ -8,6 +11,54 @@ def write_tasks_to_json(tasks, output_file):
         tasks: list of task dictionaries
         output_file: path to output file
     """
-    with open(output_file, 'w') as file:
-        for obj in tasks:
-            file.write(json.dumps(obj) + '\n')
+    try:
+        logging.info("Writing tasks to JSONL file")
+        with open(output_file, 'w') as file:
+            for obj in tasks:
+                file.write(json.dumps(obj) + '\n')
+    except Exception as e:
+        logging.error(f"Error writing tasks to JSONL file: {e}")
+
+
+def writing_batch_results(model, result):
+    """
+    Write the batch results to a file.
+    Args:
+        model: the model used for the batch job
+        result: the result to write
+    """
+    try:
+        logging.info("Writing batch results")
+        result_file_name = f"batch_outputs_Gpt/{model}_batch_results.jsonl"
+
+        with open(result_file_name, 'wb') as file:
+            file.write(result)
+
+        return result_file_name
+    except Exception as e:
+        logging.error(f"Error writing batch results: {e}")
+        return None
+    
+
+
+def reading_retrieved_results(model, batch_job):
+    """
+    Read the retrieved results from a batch job.
+    Args:
+        model: the model used for the batch job
+        batch_job: the batch job to read results from
+    """
+    try:
+        results = retrieve_batch_results(model, batch_job)
+        if results is None:
+            logging.info("No results to read")
+            return None
+        
+        for res in results[:5]:
+            task_id = res['custom_id']
+            index = task_id.split('-')[-1] # Getting index from task id
+            result = res['response']['body']['choices'][0]['message']['content']
+            logging.info(f"\n\nRESULT CONTENT for {index}: {result}")
+            logging.info("\n\n----------------------------\n\n")
+    except Exception as e:
+        print(f"Error reading retrieved results: {e}")  
